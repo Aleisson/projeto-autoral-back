5478e55af37ce52cd60b71b4992bed14a04cd96d
Aleisson <caioaleisson@gmail.com>
2023-02-13 20:57:25 -0300

    feat: controllers enums

--- src/controllers/Deputados.controllers.ts
+++ src/controllers/Deputados.controllers.ts
@@ -9,7 +9,16 @@ async function getDeputados(_req: Request, res: Response) {
     const response: deputadosList[] = await requestDeputados(URLS_CAMARA.DEPUTADOS);
 
     if (response?.length) {
-        return res.status(STATUS_CODE.OK).send(response);
+        return res.status(STATUS_CODE.OK).send(response.map(elem => ({
+            DeputadoId: elem.id,
+            uri: elem.uri,
+            nome: elem.nome,
+            siglaPartido: elem.siglaPartido,
+            uriPartido: elem.uriPartido,
+            siglaUf: elem.siglaUf,
+            fotoDeputado: elem.urlFoto,
+            emai: elem.email,
+        })));
     }
     return res.status(STATUS_CODE.NOT_FOUND).send([]);
 
