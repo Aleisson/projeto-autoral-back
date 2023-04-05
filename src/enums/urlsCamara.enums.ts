6dd2a15b342b3dfbeb8f0b5e1e56fa672939dad9
Aleisson <caioaleisson@gmail.com>
2023-04-05 00:58:17 -0300

    feat: enums and protocols

--- src/enums/urlsCamara.enums.ts
+++ src/enums/urlsCamara.enums.ts
@@ -1,5 +1,5 @@
 const URLS_CAMARA = Object.freeze({
-    DEPUTADOS: "https://dadosabertos.camara.leg.br/api/v2/deputados?siglaUf=GO&siglaUf=&dataInicio=2012-01-01&ordem=ASC&ordenarPor=nome",
+    DEPUTADOS: "https://dadosabertos.camara.leg.br/api/v2/deputados/",
 });
 
 export { URLS_CAMARA };
